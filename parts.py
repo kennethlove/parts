b89c5c21ee56a41fb69c0a5532fcb7556921392d
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-04-25 15:56:18 -0700

    Pulls websites from 5by5 network. Compatible with the last 10 shows

--- parts.py
+++ parts.py
@@ -1,9 +1,34 @@
 import requests
-from bs4 import BeautifulSoup
 from sys import argv
+from bs4 import BeautifulSoup
+import webbrowser
 
-script, url = import argv
+script, network, channel = argv
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-g_data = str(soup.find('article')).split['<h']
+
+
+class show(object):
+    def __init__(self, network, channel):
+        self.network  = network
+        self.channel =  channel
+        self.shows = []
+        self.add_shows() 
+    
+    def add_shows(self):
+        url = self.network + '/' + self.channel
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        div = soup.find('div',{'class':'box'})
+        a = div.find_all('a')
+        
+        for links in a:
+            link_check = links['href']
+            if link_check.startswith('/'+ self.channel) and not link_check.startswith('/' + self.channel + '/' + 'page'):
+                self.shows.append(self.network + links['href'])
+
+    def get_shows(self):
+        for a in self.shows:
+            print(a)
+
+tdb = show(network, channel)
+tdb.get_shows()
