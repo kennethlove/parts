bd3d27354cb378ff4d8bca434c979b589a3f81b2
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-04-25 16:11:32 -0700

    adds listing support for relay.fm

--- parts.py
+++ parts.py
@@ -6,7 +6,7 @@ import webbrowser
 script, network, channel = argv
 
 
-
+blacklist = ['feed', 'archive', 'newsletter']
 class show(object):
     def __init__(self, network, channel):
         self.network  = network
@@ -18,12 +18,13 @@ class show(object):
         url = self.network + '/' + self.channel
         r = requests.get(url)
         soup = BeautifulSoup(r.content)
-        div = soup.find('div',{'class':'box'})
-        a = div.find_all('a')
+        #div = soup.find('div',{'class':'box'})
+        #a = div.find_all('a')
+        a = soup.find_all('a') #test
         
         for links in a:
             link_check = links['href']
-            if link_check.startswith('/'+ self.channel) and not link_check.startswith('/' + self.channel + '/' + 'page'):
+            if link_check.startswith('/'+ self.channel) and not link_check.startswith('/' + self.channel + '/' + 'page') and link_check.split('/')[-1] != channel and link_check.split('/')[-1] not in blacklist:
                 self.shows.append(self.network + links['href'])
 
     def get_shows(self):
