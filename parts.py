54b5176017582ef8a47e153b3b4f9d88221acd72
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-04-26 14:11:10 -0700

    better filtering from network links

--- parts.py
+++ parts.py
@@ -6,7 +6,7 @@ import webbrowser
 script, network, channel = argv
 
 
-blacklist = ['feed', 'archive', 'newsletter']
+blacklist = ['feed', 'archive', 'newsletter',channel,'mailto']
 class show(object):
     def __init__(self, network, channel):
         self.network  = network
@@ -21,7 +21,7 @@ class show(object):
         a = soup.find_all('a')
         
         for links in a:
-            if links['href'].startswith('/'+ self.channel) and not links['href'].startswith('/' + self.channel + '/' + 'page') and links['href'].split('/')[-1] != self.channel and links['href'].split('/')[-1] not in blacklist:    
+            if links['href'].startswith('/'+ self.channel) and not links['href'].startswith('/' + self.channel + '/' + 'page') and links['href'].split('/')[-1] != self.channel and links['href'].split('/')[-1] not in blacklist and links['href'].split('//')[-1] not in blacklist:    
                 self.shows.append(self.network + links['href'])
 
     def get_shows(self):
@@ -31,9 +31,9 @@ class show(object):
 podcast = show(network, channel)
 #podcast.get_shows()
 
-r =  requests.get(podcast.shows[0])
-soup = BeautifulSoup(r.content)
+re =  requests.get(podcast.shows[0])
+soup = BeautifulSoup(re.content)
 links = soup.find_all('a')
 for link in links:
-    if not link['href'].startswith('/') and not link['href'].startswith('/' + channel + '/' + 'page') and link['href'].split('/')[-1] != channel and link['href'].split('/')[-1] not in blacklist:
+    if not link['href'].startswith('/') and not link['href'].startswith('/' + channel + '/' + 'page') and link['href'].split('/')[-1] != channel and link['href'].split('/')[-1] not in blacklist and not link['href'].split(':')[0] in blacklist and not link['href'].startswith(network) and not network.split('.')[1] in link['href'].split('.'):
         print(link['href'])
