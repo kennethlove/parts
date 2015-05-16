07629635862435b8bb89661d941a27ae93bf8230
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-05-15 19:24:59 -0700

    changes to where the "/" lives. now it lives in the code param instead of the network[url].\n NEW FEATURE: show.episodes gives you the available episodes

--- new_shownotes.py
+++ new_shownotes.py
@@ -3,23 +3,34 @@ from bs4 import BeautifulSoup as soup
 
 networks = {}
 networks['5by5'] = {
-    'url':'http://5by5.tv/',
+    'url':'http://5by5.tv',
     'content_header':'<content_sub1>'
     }
     
 class show:
-    def __init__(self,name,network,code = ''):
+    def __init__(self,name,network,code = '/'):
         self.name = name
         self.network = network
         self.code = self.get_code(code)
         self.url = networks[network]['url'] + self.code
-    
+        self.episodes = self.get_episodes(self.url, self.code)    
+
     def get_code(self,code):
-        if not code:
-            return(self.name)
+        if code == '/':
+            return(code + self.name)
+        elif not code.startswith('/'):
+            return('/' + code)
         else:
             return(code)
 
-supercharged = show('supercharged','5by5')
-print(supercharged.url)
+    def get_episodes(self, url, code):
+        r = requests.get(url)
+        bs = soup(r.content)
+        hrefs = bs.findAll('a')
+        return([link['href'] for link in hrefs if link['href'].startswith(code) or link['href'].startswith(url)])
+            
+        return(bs)
+
+dbh = show('dbh','5by5')
+print(dbh.episodes)    
 
