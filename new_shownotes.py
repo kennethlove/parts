29d210366d21aa41ec56a3fbf0f1c1f5f1072aae
kjaymiller <kjaymiller@gmail.com>
2015-05-18 15:45:54 -0700

    nested things into dictionaries

--- new_shownotes.py
+++ new_shownotes.py
@@ -1,14 +1,38 @@
-class Network:
-    def __init__(self, url, content):
-        self.url = url
-        self.content = content
-    
-    def get_podcast_url(self, podcast):
-        return(self.url + podcast)
+import requests
+from bs4 import BeautifulSoup
 
-class Podcast:
-    def __init__(self, url):
+class Network:
+    def __init__(self, name, url, class_tag, show_list):
+        self.name = name
         self.url = url
+        self.class_tag = class_tag
+        self.show_list = {Podcast(x, self.get_url(x), self.get_ep_list(self.get_url(x), x)) for x in show_list}
+        
+    def __str__(self):
+        return(self.name)
     
-fivebyfive = Network('http://5by5.tv/', 'content')
-print(fivebyfive) 
+    def get_url(self, podcast):
+        return(self.url + podcast)
+       
+    def get_ep_list(self, show_url, show_name):
+        bad_words = ['page']
+        show_list = []
+        web = requests.get(show_url)
+        soup = BeautifulSoup(web.content)
+        wrapper = soup.find("div", self.class_tag)
+        for links in wrapper.findAll('a'):
+            link = links['href']
+            if link.startswith('/{}/'.format(show_name)):
+                for word in bad_words:
+                    if word not in link:
+                        show_list.append(link)
+        return(show_list)
+        
+class Podcast:
+    def __init__(self, name, url, ep_list):
+        self.name = name
+        self.url = url
+        self.ep_list = ep_list
+
+fivebyfive = Network('5by5','http://5by5.tv/', 'box', ['dbh', 'b2w', 'supercharged'])      
+print(fivebyfive.show_list['dbh'].url)
