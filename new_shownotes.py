200ec990febd7299a72248bb69657a0b5b54fdcb
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-05-18 13:41:17 -0700

    restructure class methods

--- new_shownotes.py
+++ new_shownotes.py
@@ -1,36 +1,14 @@
-import requests
-from bs4 import BeautifulSoup as soup
-
-networks = {}
-networks['5by5'] = {
-    'url':'http://5by5.tv',
-    'content_header':'<content_sub1>'
-    }
+class Network:
+    def __init__(self, url, content):
+        self.url = url
+        self.content = content
     
-class show:
-    def __init__(self,name,network,code = '/'):
-        self.name = name
-        self.network = network
-        self.code = self.get_code(code)
-        self.url = networks[network]['url'] + self.code
-        self.episodes = self.get_episodes(self.url, self.code)    
+    def get_podcast_url(self, podcast):
+        return(self.url + podcast)
 
-    def get_code(self,code):
-        if code == '/':
-            return(code + self.name)
-        elif not code.startswith('/'):
-            return('/' + code)
-        else:
-            return(code)
-
-    def get_episodes(self, url, code):
-        r = requests.get(url)
-        bs = soup(r.content)
-        hrefs = bs.findAll('a')
-        return([link['href'] for link in hrefs if link['href'].startswith(code) or link['href'].startswith(url)])
-            
-        return(bs)
-
-dbh = show('dbh','5by5')
-print(dbh.episodes)    
-
+class Podcast:
+    def __init__(self, url):
+        self.url = url
+    
+fivebyfive = Network('http://5by5.tv/', 'content')
+print(fivebyfive) 
