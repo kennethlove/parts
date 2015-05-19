29184c9bfcc0c6d89de8b4a53b6607c4452149e2
kjaymiller <kjaymiller@gmail.com>
2015-05-19 15:43:39 -0700

    now compiles entire known_networks list

--- new_shownotes.py
+++ new_shownotes.py
@@ -25,10 +25,13 @@ class Network:
         wrapper = soup.find("div", self.class_tag)
         for links in wrapper.findAll('a'):
             link = links['href']
-            if link.startswith('/{}/'.format(show_name)):
+            if link.startswith('/{}/'.format(show_name)) or link.startswith(self.url.rsplit('/',1)[0]):
                 for word in bad_words:
                     if word not in link:
-                        show_list.append(link)
+                        if link.startswith(self.url):
+                            show_list.append(link)
+                        else:
+                            show_list.append(self.url + link)
         return(show_list)
         
 class Podcast:
@@ -38,12 +41,14 @@ class Podcast:
         self.ep_list = ep_list
 
 filename = 'overcast.opml'
-known_networks = {'5by5.tv':([],'5by5', 'box', 'http://5by5.tv'),
-                      'relay.fm':([],'test'),
-                      'rainmaker.fm':([],'test')
+known_networks = {'5by5.tv':([], '5by5', 'box', 'http://5by5.tv'),
+                      'relay.fm':([], 'relay.fm', 'small-12 medium-8 columns episode__listing', 'http://relay.fm'),
+                      'rainmaker.fm':([], 'rainmaker.fm', 'content-sidebar-wrap', 'http://rainmaker.fm/series')
                       }
-    
+
 podcast_list = network_selection(get_opml(filename), known_networks)
-fivebyfive = Network(podcast_list['5by5.tv'])
-for x in fivebyfive.show_list:
-    print(fivebyfive.show_list[x].ep_list)
+for network in podcast_list:
+    if network != 'misc':
+        cast_network = Network(podcast_list[network])
+        for x in cast_network.show_list:
+            print(cast_network.show_list[x].ep_list)
